6121348a8a6011dc8c10f0c864d932d7d1a9f61a
samberubz <141795376+samberubz@users.noreply.github.com>
2023-09-27 21:01:01 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,44 +1,27 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import yfinance as yf
 import streamlit as st
 from streamlit.logger import get_logger
-
 LOGGER = get_logger(__name__)
-
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Dashboard",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Making money should be free 👋")
 
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.success("Select a tab above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
+        samberubz2023invest is an open-source app built specifically for
+        Stock Investing (NYSE, NASDAQ and TSX) and for the use of AI.
+        **👈 Select a tab from the sidebar** to start investing!
+        ### Want to fund this project?
+        - You can reach out to me via Linkedin [Linkedin](www.linkedin.com/in/samuel-bérubé-21b1a0127)
+        - 
+        
         ### See more complex demos
         - Use a neural net to [analyze the Udacity Self-driving Car Image
           Dataset](https://github.com/streamlit/demo-self-driving)
